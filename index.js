790eea5d019071dd32635ddd342257da86d27364
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-07-11 14:30:09 +0300

    Context

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Your code here
+function createEmployeeRecord(array) {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(' ');
+  
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date
+    });
+  
+    return employee;
+  }
+  
+  function createTimeOutEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(' ');
+  
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date
+    });
+  
+    return employee;
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    let timeInEvent = employee.timeInEvents.find(e => e.date === date);
+    let timeOutEvent = employee.timeOutEvents.find(e => e.date === date);
+  
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    let hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  function allWagesFor(employee) {
+    let dates = employee.timeInEvents.map(e => e.date);
+  
+    return dates.reduce((total, date) => {
+      return total + wagesEarnedOnDate(employee, date);
+    }, 0);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => {
+      return total + allWagesFor(employee);
+    }, 0);
+  }
+  
